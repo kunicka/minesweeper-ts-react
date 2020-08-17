4f1ea694525a13e4d93eadaadc03f004d78878b0
Agnieszka <kuncika2000@gmail.com>
2020-08-17 17:14:05 +0200

    app display is ready

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.scss";
+import { NumberDisplay } from "../NumberDisplay/NumberDisplay";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="Header">
+        <NumberDisplay value={0}/>
+        <div className="Face"><span role="img" aria-label="face">😉</span></div>
+         <NumberDisplay value={23}/>
+      </div>
+      <div className="Body">Body</div>
+    </div>
+  );
+};
+
+export default App;
